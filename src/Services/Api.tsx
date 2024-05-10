ab51749d3b17ca27b78a4f7aeb48813b68236d66
fabricio <fzabra@gmail.com>
2024-05-10 06:06:31 -0300

    add comment code

--- src/Services/Api.tsx
+++ src/Services/Api.tsx
@@ -3,9 +3,13 @@ import { ResponseError } from './ResponseError';
 
 const API_URL = 'https://api.github.com/';
 
-
+// This function fetchData is an asynchronous function that fetches data from the provided URL.
+// It takes a single argument:
+// - url: a string representing the URL from which to fetch the data
+// The function returns a Promise that resolves to the fetched data.
 async function fetchData(url: string): Promise<any> {
   try {
+    // Send a GET request to the specified URL using the fetch API.
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error('Failed to fetch data');
@@ -16,6 +20,8 @@ async function fetchData(url: string): Promise<any> {
   }
 }
 
+
+//function to find public repositories
 export async function getRepos(query: string): Promise<InterfacePublicRepo[] | ResponseError> {
   const reposFilteredURL = `${API_URL}search/repositories?q=${query}{&page,per_page,sort,order}`;
   try {
@@ -28,7 +34,7 @@ export async function getRepos(query: string): Promise<InterfacePublicRepo[] | R
 }
 
 
-
+//function to get repositorie detail
 export async function getRepoDetails(ownerLogin: string, repoName: string): Promise<InterfacePublicRepo | null> {
   const repoDetailsURL = `${API_URL}repos/${ownerLogin}/${repoName}`;
 
